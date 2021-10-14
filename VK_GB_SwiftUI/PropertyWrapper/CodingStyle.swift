dc747f831be83687a0f37f9f21a3428d17cfa62e
chernyaevpavel <pawliklist@yandex.ru>
2021-10-14 10:59:06 +0400

    propertyWrapper

---
+++ VK_GB_SwiftUI/PropertyWrapper/CodingStyle.swift
@@ -0,0 +1,65 @@
+//
+//  CodingStyle.swift
+//  VK_GB_SwiftUI
+//
+//  Created by Павел Черняев on 13.10.2021.
+//
+
+//import Foundation
+
+enum Style: String {
+    case camelCase = ""
+    case snakeCase = "_"
+    case kebabCase = "-"
+}
+
+@propertyWrapper
+struct CodingStyle {
+    
+    private var value: String
+    private let style: Style
+
+    init(wrappedValue: String, style: Style) {
+        self.value = wrappedValue
+        self.style = style
+    }
+    
+    public var wrappedValue: String {
+        get {
+            get()
+        }
+        set {
+            set(newValue)
+        }
+    }
+    
+    //MARK:- private
+    private func get() -> String {
+        return format(value)
+    }
+    
+    private mutating func set(_ newValue: String) {
+        self.value = newValue
+    }
+    
+    private func format(_ string: String) -> String {
+        var tmpStr = string
+            .replacingOccurrences(of: Style.snakeCase.rawValue, with: " ")
+            .replacingOccurrences(of: Style.kebabCase.rawValue, with: " ")
+            .lowercased()
+        
+        var arr = tmpStr
+            .split(separator: " ")
+            .map { String($0) }
+        
+        if style == .camelCase {
+            arr = arr.map { $0.capitalized }
+            if !arr.isEmpty {
+                arr[0] = arr[0].lowercased()
+            }
+        }
+        
+        tmpStr = arr.joined(separator: style.rawValue)
+        return tmpStr
+    }
+}
--- VK_GB_SwiftUI/UserStories/Main/ContentView.swift
+++ VK_GB_SwiftUI/UserStories/Main/ContentView.swift
@@ -11,6 +11,21 @@ struct ContentView: View {
     
     @State var isLoginComplete = false
     
+    //test @CodingStyle
+//    init() {
+//        @CodingStyle(style: .camelCase) var myString = "test Test  test QWECSDED"
+//        print(myString)
+//
+//        @CodingStyle(style: .snakeCase) var myString1 = "test Test-  test QWECSDED"
+//        print(myString1)
+//
+//        @CodingStyle(style: .kebabCase) var myString2 = "test Test_  test QWECSDED"
+//        print(myString2)
+//
+//        @CodingStyle(style: .camelCase) var myString3 = " "
+//        print(myString3)
+//    }
+    
     var body: some View {
         NavigationView {
             HStack {
@@ -23,7 +38,7 @@ struct ContentView: View {
                     label: {
                         EmptyView()
                     })
-                    
+
             }
             .navigationBarHidden(true)
         }
